897fb8730e4b1ce4a8723d3928349420203f3d97
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-01-08 23:16:51 +0200

    tailwind

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 export const App = () => {
   return (
     <div
+      className="bg-slate-500"
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
