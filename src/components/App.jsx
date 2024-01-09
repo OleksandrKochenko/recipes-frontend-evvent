34cf5be349f326b55289415c0a79032adc0e9a4a
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-01-09 03:39:58 +0200

    refresh user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { Layout } from './Layout';
 import { Home } from './pages/home';
 import { SignIn } from './pages/signin';
-import { NotFound } from './pages/not-found';
 import { SignUp } from './pages/signup';
+import { NotFound } from './pages/not-found';
+import { refreshUser } from '../redux/auth/operations';
+import { Loader } from './Loader';
 
 export const App = () => {
-  return (
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  const isLoading = useSelector(state => state.auth.isLoading);
+
+  return isLoading ? (
+    <Loader />
+  ) : (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
