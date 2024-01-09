28237a1b5d4845c145fb20facc332b96988d044b
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-01-09 12:36:09 +0200

    fix warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ export const App = () => {
   const isLoading = useSelector(state => state.auth.isLoading);
   const isRefreshing = useSelector(state => state.auth.isRefreshing);
 
-  return isRefreshing ? (
+  return isRefreshing || isLoading ? (
     <Loader />
   ) : (
     <Routes>
