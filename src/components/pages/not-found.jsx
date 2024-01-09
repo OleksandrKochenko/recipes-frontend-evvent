e9cd5bad6c76c317e51d7263c639d23f69455931
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-01-09 03:44:48 +0200

    go home btn

--- src/components/pages/not-found.jsx
+++ src/components/pages/not-found.jsx
@@ -1,13 +1,20 @@
 import { Button } from 'components/elements/button';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return (
     <div className="h-screen w-screen absolute top-0 left-0 flex flex-col justify-center items-center">
       <h1 className="text-[180px] font-semibold m-0">404</h1>
       <p className="text-xl bg-orange-700 p-2 rounded-md fixed top-[calc(1/2-20px)] left-[calc(1/2-20px)] rotate-12">
         Page Not Found
       </p>
-      <Button title="Go home" />
+      <Button title="Go home" onClick={handleClick} />
     </div>
   );
 };
